cec9b5bb96a287a1da0273906531e355d08f217e
Shubroto Shuvo <shubroto.debnath@gmail.com>
2026-09-06 17:56:03 -0400

    added context for sharing notes

---
+++ ui/src/context/NotesContext.tsx
@@ -0,0 +1,53 @@
+import { createContext, useContext, useState } from "react";
+import { Note, Task } from "../types/task";
+
+// create values to be shared with whoever consuming the context
+interface NotesContextValue {
+    notes: Note[];
+    tasks: Task[];
+    addNote: (title: string) => void;
+    deleteNote: (noteId: number) => void;
+    addTask: (value: string, noteId: number) => void;
+    deleteTask: (id: number) => void;
+}
+
+// create a context that will be channeled to components consuming it
+const NotesContext = createContext<NotesContextValue | undefined>(undefined);
+
+// create a broadcaster or context provider that will supply the context values to its children components
+export function NotesProvider({ children }: { children: React.ReactNode }) {
+    const [notes, setNotes] = useState<Note[]>([])
+    const [tasks, setTasks] = useState<Task[]>([])
+
+    const addNote = (title: string) => {
+        setNotes([...notes, { id: Date.now(), title: title, timeStamp: Date.now() }])
+    }
+
+    const deleteNote = (noteId: number) => {
+        setNotes(notes.filter(note => note.id !== noteId))
+    }
+
+    const addTask = (value: string, noteId: number) => {
+        const task = { id: Date.now(), content: value, timeStamp: Date.now(), noteId: noteId }
+        setTasks([...tasks, task])
+    }
+
+    const deleteTask = (id: number) => {
+        setTasks(prev => prev.filter((item) => item.id !== id))
+    }
+
+    // everyone consuming this context will have access to the values.
+    return (
+        <NotesContext.Provider value={{ notes, tasks, addNote, deleteNote, addTask, deleteTask }}>
+            {children}
+        </NotesContext.Provider>
+    )
+}
+
+// creating a custom hook that allows components to access the NotesContext values easily
+// and throws an error if used outside of the NotesProvider.
+export function useNotesContext() {
+    const context = useContext(NotesContext)
+    if (!context) throw new Error('useNotesContext must be within NotesProvider')
+    return context
+}
